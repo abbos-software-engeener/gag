4135be96ef494cc65cf7c085e83c5bfc0201aa04
ilovedarknet <abboscik990@gmail.com>
2021-10-12 17:32:14 +0500

    updated

--- gag/settings.py
+++ gag/settings.py
@@ -75,7 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django_settings_export.settings_export',
+                # 'django_settings_export.settings_export',
                 'main.content_processors.categories',
             ],
         },
